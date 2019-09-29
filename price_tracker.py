f2fb06db966479317220e9802badc5c2a902ef60
Oleh Sabaniuk <olega007@gmail.com>
2019-09-29 23:02:41 +0300

    moved from openpyxl to gspread

--- price_tracker.py
+++ price_tracker.py
@@ -6,8 +6,8 @@ your price. all data is stored in google spreadsheets'''
 import requests
 import re
 import bs4
-import openpyxl
-from openpyxl.utils import get_column_letter
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import smtplib
 import time
 from colorama import Fore, Style
@@ -17,8 +17,7 @@ class PriceTracker(object):
     def __init__(self, shop_list_file):
         self.shop_list_file = shop_list_file
 
-    def scrap_price(self, row_nubmer, shop_link, your_price, email):
-        print(f'Row #{Style.BRIGHT}{row_nubmer}{Style.RESET_ALL}')
+    def scrap_price(self, shop_link, your_price, email):
         print(f'Checking price of product at {Style.BRIGHT}"{shop_link}"\
 {Style.RESET_ALL}')
         res = requests.get(shop_link)
@@ -54,36 +53,71 @@ sent.\n{Style.RESET_ALL}')
         self.smtp_connect()
         regex_url = re.compile(r'^(?:http|ftp)s?://')
         regex_email = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-        wb = openpyxl.load_workbook(filename=self.shop_list_file)
-        ws = wb.active
-        for row in ws.iter_rows(min_row=2, max_col=6, max_row=ws.max_row):
-            row_number = row[0].value
-            row_url = row[1].value
-            row_price = row[2].value
-            row_email = row[3].value
-            if row_url is None:
-                pass
-            elif not (re.search(regex_url, row_url)):
-                error = 'The url should start from http:// or https://'
-                row[5].value = error
-                print(f'{Fore.RED}{error}{Style.RESET_ALL}\n')
-            elif row_price is None or str(type(row_price)) not in ("<class 'int'>", "<class 'float'>",
-                                            "<class 'NoneType'>"):
-                error = 'The price must be integer or float'
-                row[5].value = error
-                print(f'{Fore.RED}{error}{Style.RESET_ALL}\n')
-            elif row_email is None or not (re.search(regex_email, row_email)):
-                error = 'Wrong email'
-                row[5].value = error
-                print(f'{Fore.RED}{error}{Style.RESET_ALL}\n')
-            elif row[4].value is None or row[4].value > 0:
-                result = self.scrap_price(row_number, row_url, row_price, row_email)
-                if result is True:
-                    row[4].value -= 1
+        # use creds to create a client to interact with the Google Drive API
+        scope = ['https://spreadsheets.google.com/feeds']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('rivne-price-tracker-0ea490480fcf.json', scope)
+        client = gspread.authorize(creds)
+        #work with spreadsheet
+        wb = client.open_by_url('https://docs.google.com/spreadsheets/d/1Cv-zzL2YXqEizoH-ewQ0athDOtcjDL_T9xfNbc2YzUE/edit#gid=0')
+        ws = wb.worksheet('list')
+        max_rows = len(ws.get_all_values())
+
+        for row in range(2, max_rows + 1):
+            error = ''
+            values = ws.row_values(row)
+            if len(values) >= 5:
+                row_number = values[0]
+                print(f'Row #{Style.BRIGHT}{row_number}{Style.RESET_ALL}')
+
+                if values[1]:
+                    row_url = values[1]
                 else:
-                    row[5].value = result
+                    row_url = ""
+
+                if values[2]:
+                    try:
+                        row_price = float(values[2].replace(',', '.'))
+                    except:
+                        error = 'The price must be integer or float'
+                        ws.update_cell(row, 6, error)
+                        print(f'{Fore.RED}{error}{Style.RESET_ALL}\n')
+                else:
+                    row_price = 0
+
+                if values[3] and (re.search(regex_email, values[3])):
+                    row_email = values[3]
+                else:
+                    row_email = ""
+                    error = 'Wrong or empty email'
+                    ws.update_cell(row, 6, error)
+                    print(f'{Fore.RED}{error}{Style.RESET_ALL}\n')
+
+                if values[4]:
+                    try:
+                        row_repeat = int(values[4])
+                    except:
+                        row_repeat = 0
+                        error = 'The repeat number must be integer'
+                        ws.update_cell(row, 6, error)
+                        print(f'{Fore.RED}{error}{Style.RESET_ALL}\n')
+                else:
+                    row_repeat = 0
+
+                if row_url == "":
+                    pass
+                elif not (re.search(regex_url, row_url)):
+                    error = 'The url should start from http:// or https://'
+                    ws.update_cell(row, 6, error)
+                    print(f'{Fore.RED}{error}{Style.RESET_ALL}\n')
+                elif error == '' and row_repeat > 0:
+                    result = self.scrap_price(row_url, row_price, row_email)
+                    if result is True:
+                        ws.update_cell(row, 5, row_repeat - 1)
+                    else:
+                        ws.update_cell(row, 6, result)
+            else:
+                print(f"{Fore.RED}in row number {row - 1} some fields are empty{Style.RESET_ALL}")
         self.disconnect_smtp()
-        wb.save(self.shop_list_file)
 
     def smtp_connect(self):
         self.smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
@@ -110,5 +144,5 @@ while True:
     iteration_number += 1
     print(f'\n{Style.BRIGHT}Iteration #{iteration_number}{Style.RESET_ALL}\n')
     reebok_price.parse_shop_list()
-    time.sleep(3600)
+    time.sleep(5)
 
