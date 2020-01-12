d7560c32d844cef6877453feac788b8c29a96d93
RoseTracker <rive.price.tracker@gmail.com>
2020-01-12 17:16:17 +0200

    made sleep printing with \r

--- price_tracker.py
+++ price_tracker.py
@@ -305,9 +305,8 @@ class PriceTracker(object):
         sleep_time = round(rest_time / 60)
         for i in range(sleep_time, -1, -1):
             if i > 0:
-                print(f'{Fore.YELLOW}Next iteration will start in {i} minutes{Style.RESET_ALL}')
+                print(f'{Fore.YELLOW}Next iteration will start in {i} minutes{Style.RESET_ALL}', end='\r')
                 time.sleep(60)
-                print('\r')
 
 
 reebok_price = PriceTracker()
